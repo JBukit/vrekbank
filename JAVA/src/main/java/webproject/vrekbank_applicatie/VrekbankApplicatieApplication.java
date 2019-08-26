f941f84de7fcf66eb99e3be132830552c6949ebd
Jacco <jjvdheuvel@hotmail.com>
2019-08-26 15:50:01 +0200

    afspraken met productowner

---
+++ JAVA/src/main/java/webproject/vrekbank_applicatie/VrekbankApplicatieApplication.java
@@ -0,0 +1,13 @@
+package webproject.vrekbank_applicatie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrekbankApplicatieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VrekbankApplicatieApplication.class, args);
+    }
+
+}
---
+++ JAVA/src/main/java/webproject/vrekbank_applicatie/controller/AccountOverviewController.java
@@ -0,0 +1,13 @@
+package webproject.vrekbank_applicatie.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AccountOverviewController {
+
+/*    @PostMapping (value = "accountOverview")
+    public String accountOverviewHandler (){
+        return a
+    }*/
+}
