440293d7e7854452071f714f0a623b2ae2594478
Sven Spithoven <svenspithoven@me.com>
2019-08-28 14:57:22 +0200

    Aanpassingen

--- JAVA/src/main/java/webproject/vrekbank_applicatie/controller/RegistrationController.java
+++ JAVA/src/main/java/webproject/vrekbank_applicatie/controller/RegistrationController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class RegisterController {
+public class RegistrationController {
 
     @GetMapping(value = "Registration")
-    public String indexRegisterHandler() {
+    public String indexRegistrationHandler() {
         return "RegistrationConfirmation";
     }
 }
