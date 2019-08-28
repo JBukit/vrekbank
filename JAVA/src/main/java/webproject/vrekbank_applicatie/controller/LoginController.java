02536574b402ca4b9c0af6cf07e4ccff8f2952fb
Elleke <elleke.van.de.pol@hva.nl>
2019-08-28 14:16:20 +0200

    werken aan Overview

--- JAVA/src/main/java/webproject/vrekbank_applicatie/controller/LoginController.java
+++ JAVA/src/main/java/webproject/vrekbank_applicatie/controller/LoginController.java
@@ -4,12 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import webproject.vrekbank_applicatie.model.Account;
+import webproject.vrekbank_applicatie.model.PersonalAccount;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class LoginController {
 
     @GetMapping(value = "Overview")
-    public String loginHandler () {
+    public String loginOverviewHandler (Model model) {
+        // make lists with accounts, to be added from database
+        List<PersonalAccount> accounts = new ArrayList<>();
+
+        // add accounts of customer (from database, now by hand)
+        accounts.add(new PersonalAccount(1,"NL1",10,0,false));
+        accounts.add(new PersonalAccount(2,"NL2",20,0,false));
+        accounts.add(new PersonalAccount(3,"NL3",30,0,false));
+
+        // add list to .html
+        model.addAttribute("allAccounts",accounts);
+
         return "Overview";
     }
 
