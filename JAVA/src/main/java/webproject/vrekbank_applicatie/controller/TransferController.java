4cf58101c6e268a7eb827b3333f35455e52f6f7a
Tristan <tristanwesenhagen@gmail.com>
2019-08-28 12:25:10 +0200

    Beginnetje scherm geld overmaken

---
+++ JAVA/src/main/java/webproject/vrekbank_applicatie/controller/TransferController.java
@@ -0,0 +1,8 @@
+package webproject.vrekbank_applicatie.controller;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TransferController {
+
+//    public String transferHandler{}
+}
