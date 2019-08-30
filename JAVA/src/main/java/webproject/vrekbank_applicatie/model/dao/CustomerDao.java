c4efedea4852ce663091c7a0e9221d41c83a3c50
Elleke <elleke.van.de.pol@hva.nl>
2019-08-30 15:23:54 +0200

    Customer: Entity, Service/validator, Dao

--- JAVA/src/main/java/webproject/vrekbank_applicatie/model/dao/CustomerDao.java
+++ JAVA/src/main/java/webproject/vrekbank_applicatie/model/dao/CustomerDao.java
@@ -1,4 +1,11 @@
 package webproject.vrekbank_applicatie.model.dao;
 
-public interface CustomerDao {
+import org.springframework.data.repository.CrudRepository;
+import webproject.vrekbank_applicatie.model.Customer;
+
+public interface CustomerDao extends CrudRepository<Customer, Integer> {
+
+    // additional methodes to check/update in database
+
+
 }
--- JAVA/src/main/java/webproject/vrekbank_applicatie/service/CustomerValidator.java
+++ JAVA/src/main/java/webproject/vrekbank_applicatie/service/CustomerValidator.java
@@ -1,4 +1,25 @@
 package webproject.vrekbank_applicatie.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import webproject.vrekbank_applicatie.model.dao.CustomerDao;
+
+@Service
 public class CustomerValidator {
+
+    @Autowired
+    CustomerDao customerDao;
+
+    // no-args constructor
+
+    public CustomerValidator() {
+        super();
+    }
+
+
+    // methods to add, check, update, delete customers
+
+
+
+
 }
