af966f07280cdc074ca74267316fd184422acc8f
Tristan <tristanwesenhagen@gmail.com>
2019-09-02 15:52:50 +0200

    Update Transfer, delete van Debit en Credit, en aanmaken van TransferValidator

--- JAVA/src/main/java/webproject/vrekbank_applicatie/model/Transfer.java
+++ JAVA/src/main/java/webproject/vrekbank_applicatie/model/Transfer.java
@@ -1,20 +1,26 @@
 package webproject.vrekbank_applicatie.model;
 
-abstract class Transfer {
+public class Transfer {
     // variables
     private int transactionNr;
     private String description;
     private String date;
+    private String debitIban;
+    private String creditIban;
+    private double transferamount;
 
     // constructors
     public Transfer() {
     }
 
-    public Transfer(int transactionNr, String description, String date) {
+    public Transfer(int transactionNr, String description, String date, String debitIban, String creditIban, double transferamount) {
         super();
         this.transactionNr = transactionNr;
         this.description = description;
         this.date = date;
+        this.debitIban = debitIban;
+        this.creditIban = creditIban;
+        this.transferamount = transferamount;
     }
 
     // getters and setters
@@ -30,4 +36,12 @@ abstract class Transfer {
     public void setDescription(String description) {
         this.description = description;
     }
+    public String getDate() { return date; }
+    public void setDate(String date) { this.date = date; }
+    public String getDebitIban() { return debitIban; }
+    public void setDebitIban(String debitIban) { this.debitIban = debitIban; }
+    public String getCreditIban() { return creditIban; }
+    public void setCreditIban(String creditIban) { this.creditIban = creditIban; }
+    public double getTransferamount() { return transferamount; }
+    public void setTransferamount(double transferamount) { this.transferamount = transferamount; }
 }
