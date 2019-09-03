e10e72b24876ac5628db8aa563c66f84f4e21575
Elleke <elleke.van.de.pol@hva.nl>
2019-09-03 10:29:26 +0200

    linken naar transfer scherm

--- JAVA/src/main/java/webproject/vrekbank_applicatie/controller/TransferController.java
+++ JAVA/src/main/java/webproject/vrekbank_applicatie/controller/TransferController.java
@@ -2,15 +2,20 @@ package webproject.vrekbank_applicatie.controller;
 
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import webproject.vrekbank_applicatie.model.Customer;
 import webproject.vrekbank_applicatie.model.Transfer;
 
 
-//@Controller
-//public class TransferController {
-//
+@Controller
+public class TransferController {
+
+    @GetMapping(value = "transaction")
+    public String transferHandler() {
+        return "Transfer";
+
 //    @PostMapping(value = "confirm")
 //    public String transferConfirmationTransferHandler (@ModelAttribute Transfer transfer, Model model){
 //
@@ -27,8 +32,8 @@ import webproject.vrekbank_applicatie.model.Transfer;
 //        //3. Iban ontvanger moet valide zijn, bestaat de iban?
 //        //Indien niet aan een voorwaarde voldaan: Pagina wordt herladen en gebruiker moet het opnieuw invullen
 //
-//    }
-//}
+    }
+}
 
 
 
