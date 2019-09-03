f5451f0f9cc79e56a6446c1f19fd012de75ab457
Elleke <elleke.van.de.pol@hva.nl>
2019-09-03 09:31:23 +0200

    check inlog username en pw in database

--- JAVA/src/main/java/webproject/vrekbank_applicatie/model/dao/CustomerDao.java
+++ JAVA/src/main/java/webproject/vrekbank_applicatie/model/dao/CustomerDao.java
@@ -3,9 +3,13 @@ package webproject.vrekbank_applicatie.model.dao;
 import org.springframework.data.repository.CrudRepository;
 import webproject.vrekbank_applicatie.model.Customer;
 
+import java.util.List;
+
 public interface CustomerDao extends CrudRepository<Customer, Integer> {
 
     // additional methodes to check/update in database
+
+    public Customer findByUsername(String name);
 
 
 }
--- JAVA/src/main/java/webproject/vrekbank_applicatie/service/CustomerValidator.java
+++ JAVA/src/main/java/webproject/vrekbank_applicatie/service/CustomerValidator.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import webproject.vrekbank_applicatie.model.Customer;
 import webproject.vrekbank_applicatie.model.dao.CustomerDao;
 
+import java.util.List;
+
 @Service
 public class CustomerValidator {
 
@@ -21,6 +23,11 @@ public class CustomerValidator {
     // methods to add, check, update, delete customers
     public void saveCustomer (Customer customer) {
         customerDao.save(customer);
+    }
+
+    public Customer findCustomerByUsername (String name) {
+        Customer c = customerDao.findByUsername(name);
+        return c;
     }
 
 
