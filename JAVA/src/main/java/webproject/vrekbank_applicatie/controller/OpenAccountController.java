8c14a937c9b5b718e7190b1c0db7f8c786e6b3b6
Jacco <jjvdheuvel@hotmail.com>
2019-09-03 13:06:05 +0200

    open new account confirmation in schermen voor part. en MKB

--- JAVA/src/main/java/webproject/vrekbank_applicatie/controller/OpenAccountController.java
+++ JAVA/src/main/java/webproject/vrekbank_applicatie/controller/OpenAccountController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import webproject.vrekbank_applicatie.model.Account;
+import webproject.vrekbank_applicatie.model.BusinessAccount;
 import webproject.vrekbank_applicatie.model.Customer;
 import webproject.vrekbank_applicatie.model.PersonalAccount;
 
@@ -13,10 +14,15 @@ import static webproject.vrekbank_applicatie.model.Account.CreateIBAN;
 @Controller
 public class OpenAccountController {
 
+    //NOG BEDENKEN: hoe account in DB koppelen aan klant (via session?)
+    // HOE COMPANY invoegen in business rekening? Is dat eerder aan session geladen?
+
+
     // handler voor particulier rekening. later evt aan te passen tot 1 handler voor ook MKB,
     // die via bijv radio knop beide subklassen kan aanmaken
-    @PostMapping(value = "OpenAccountConfirmation")
-    public String OpenAccountOpenAccountConfirmationHandler(@ModelAttribute PersonalAccount personalAccount, Model model) {
+
+    @PostMapping(value = "OpenPersonalAccountConfirmation")
+    public String OpenAccountOpenPersonalAccountConfirmationHandler(@ModelAttribute PersonalAccount personalAccount, Model model) {
 
         // fill in new account/object
 
@@ -34,13 +40,39 @@ public class OpenAccountController {
         model.addAttribute("rekeningnummer", personalAccount.getIBAN());
 
 
+        // add accountinfo to database
+
+        // go to OpenAccountConfirmationScreen
+        return "OpenPersonalAccountConfirmation";
+    }
+
+    // tweede handler voor MKB rekeningen. NOG DOEN: set company
+    @PostMapping(value = "OpenBusinessAccountConfirmation")
+    public String OpenAccountOpenBusinessAccountConfirmationHandler(@ModelAttribute BusinessAccount businessAccount,
+                                                                    Model model) {
+
+        // fill in new account/object
+
+        //model.addAttribute("businessAccount", account.setBusinessAccount(account.isBusinessAccount());
+
+        businessAccount.setAccountId(0); // op nul zetten, wordt door DB overschreven
+        businessAccount.setBalance(0);
+        businessAccount.setMinimumBalance(0);
+        businessAccount.setIBAN(CreateIBAN());
+        businessAccount.setBusinessAccount(true);
+        //businessAccount.setCompanyId();
 
 
+        //opnemen in  model, in ieder geval om in bevestigingsscherm gegevens terug te kunnen geven.
+        model.addAttribute("saldo", businessAccount.getBalance());
+        model.addAttribute("minimumsaldo", businessAccount.getMinimumBalance());
+        model.addAttribute("rekeningnummer", businessAccount.getIBAN());
+        model.addAttribute("bedrijf", businessAccount.getCompanyId());
 
         // add accountinfo to database
 
         // go to OpenAccountConfirmationScreen
-        return "OpenAccountConfirmation";
+        return "OpenBusinessAccountConfirmation";
     }
 }
 
