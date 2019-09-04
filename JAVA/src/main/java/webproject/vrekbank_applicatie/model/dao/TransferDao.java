117f94a5c80b59f3a679d705479a992610688ab1
paulinekors <paulinekors@gmail.com>
2019-09-04 13:35:47 +0200

    AccountSummary scherm werkende + update TransferValidator + update TransferDao

--- JAVA/src/main/java/webproject/vrekbank_applicatie/model/dao/TransferDao.java
+++ JAVA/src/main/java/webproject/vrekbank_applicatie/model/dao/TransferDao.java
@@ -4,12 +4,14 @@ import org.springframework.data.repository.CrudRepository;
 import webproject.vrekbank_applicatie.model.Account;
 import webproject.vrekbank_applicatie.model.Transfer;
 
+import java.util.List;
+
 public interface TransferDao extends CrudRepository<Transfer, Integer> {
 
         // additional methodes to check/update in database
 
-        public Transfer findByCreditIban (String creditIban);
-        public Transfer findByDebitIban (String debitIban);
+        public List <Transfer> findByCreditIban (String creditIban);
+        public List <Transfer> findByDebitIban (String debitIban);
 
 
 }
