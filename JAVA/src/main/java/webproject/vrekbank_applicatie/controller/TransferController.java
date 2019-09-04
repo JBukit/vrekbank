50ebf92ddc5ca9e51de69bab0661d529e8d468f8
Tristan <tristanwesenhagen@gmail.com>
2019-09-04 13:26:15 +0200

    TransferController aangemaakt met Handler en modelAttributes

---
+++ JAVA/src/main/java/webproject/vrekbank_applicatie/controller/TransferController.java
@@ -0,0 +1,29 @@
+package webproject.vrekbank_applicatie.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import webproject.vrekbank_applicatie.model.Transfer;
+import webproject.vrekbank_applicatie.service.TransferValidator;
+
+@Controller
+@SessionAttributes
+public class TransferController {
+
+    @Autowired
+    TransferValidator transferValidator;
+
+    @PostMapping(value = "TransferConfirmation")
+    public String transferTransferConfirmation (@ModelAttribute Transfer transfer, Model model) {
+        transferValidator.saveTransfer(transfer);
+        model.addAttribute("creditIban", transfer.getCreditIban());
+        model.addAttribute("debitIban", transfer.getDebitIban());
+        model.addAttribute("transferamount",transfer.getTransferamount());
+        model.addAttribute("description", transfer.getDescription());
+        model.addAttribute("date", transfer.getDate());
+        return "TransferConfirmation";
+    }
+}
