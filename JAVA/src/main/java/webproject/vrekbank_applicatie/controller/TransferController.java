817100e5fd18b3b4c06fb883666cb34381da1d46
Jacco <jjvdheuvel@hotmail.com>
2019-09-05 17:11:31 +0200

    transfer confirmation geeft overzicht

--- JAVA/src/main/java/webproject/vrekbank_applicatie/controller/TransferController.java
+++ JAVA/src/main/java/webproject/vrekbank_applicatie/controller/TransferController.java
@@ -3,30 +3,39 @@ package webproject.vrekbank_applicatie.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import webproject.vrekbank_applicatie.model.Transfer;
 import webproject.vrekbank_applicatie.service.TransferValidator;
 
 @Controller
-@SessionAttributes
+@SessionAttributes({"name", "iban"})
 public class TransferController {
 
     @Autowired
     TransferValidator transferValidator;
 
     @PostMapping(value = "TransferConfirmation")
-    public String transferTransferConfirmation (@ModelAttribute Transfer transfer, Model model) {
-//        transferValidator.saveTransfer(transfer);
-        model.addAttribute("creditIban", transfer.getCreditIban());
-        model.addAttribute("debitIban", transfer.getDebitIban());
+    public String transferTransferConfirmation (@SessionAttribute("iban") String iban, @ModelAttribute Transfer transfer, Model model) {
+
+// poging tot doorgeven naar volgend scherm
+        model.containsAttribute("iban");
+        model.containsAttribute("name");
+
+        // model vullen uit transferobject
+        model.addAttribute("debitIban", iban); // betaler
+
+        model.addAttribute("creditIban", transfer.getCreditIban()); // ontvanger
+
 //        model.addAttribute("creditIbanName", transfer.getCreditIbanName());
 //        model.addAttribute("debitIbanName", transfer.getDebitIbanName());
+
         model.addAttribute("transferAmount",transfer.getTransferAmount());
         model.addAttribute("description", transfer.getDescription());
         model.addAttribute("date", transfer.getDate());
+
+        //uit tranfer object schriijven naar database
+       // transferValidator.saveTransfer(transfer);
+
         return "TransferConfirmation";
 
     }
