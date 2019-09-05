8ff3d7a8a3360cd8e6e30778bc51a22a0c0da1f5
Elleke <elleke.van.de.pol@hva.nl>
2019-09-05 15:42:14 +0200

    toevoeging firstName SessionAttribute aan LoginController en Overview.html

--- JAVA/src/main/java/webproject/vrekbank_applicatie/controller/LoginController.java
+++ JAVA/src/main/java/webproject/vrekbank_applicatie/controller/LoginController.java
@@ -3,11 +3,9 @@ package webproject.vrekbank_applicatie.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import webproject.vrekbank_applicatie.model.Account;
 import webproject.vrekbank_applicatie.model.BusinessAccount;
 import webproject.vrekbank_applicatie.model.Customer;
 import webproject.vrekbank_applicatie.model.PersonalAccount;
@@ -19,7 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@SessionAttributes("name")
+@SessionAttributes({"name", "firstName"})
 public class LoginController {
 
     @Autowired
@@ -40,6 +38,7 @@ public class LoginController {
             // if check = ok, proceed:
             // add Customer-name to html page to show username in page
             model.addAttribute("name", customer.getUsername());
+            model.addAttribute("firstName",c.getFirstName());
 
             // make lists with accounts, to be added from database
             List<PersonalAccount> personalAccounts = new ArrayList<>();
