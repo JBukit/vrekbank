2a4f621155b403ee7d1a956c8f5e121ec1c1a4df
Sven Spithoven <svenspithoven@me.com>
2019-09-05 14:13:52 +0200

    Customer File Launcher

---
+++ JAVA/src/main/java/webproject/vrekbank_applicatie/model/CustomerFileLauncher.java
@@ -0,0 +1,53 @@
+package webproject.vrekbank_applicatie.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import webproject.vrekbank_applicatie.service.CustomerValidator;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+@Component
+public class CustomerFileLauncher {
+
+    public static final int FILE_SIZE = 4000;
+    public static List<Customer> customerList;
+
+    @Autowired
+    CustomerValidator validator;
+
+    public static void makeCustomerList() {
+        Scanner customerReader;
+        customerList = new ArrayList<>();
+        try {
+            File fakeNames = new File("Namelist/FakeNames.csv");
+            customerReader = new Scanner(fakeNames);
+            for (int i = 0; i < FILE_SIZE; i++) {
+                String[] customerSplit = customerReader.nextLine().split(";");
+                String firstName = customerSplit[0];
+                System.out.println(firstName);
+                String suffix = customerSplit[1];
+                String lastName = customerSplit[2];
+                String address = customerSplit[3];
+                String zipcode = customerSplit[4];
+                String city = customerSplit[5];
+                String email = customerSplit[6];
+                char sex = customerSplit[7].charAt(0);
+                String dob = customerSplit[8];
+                int BSN = Integer.parseInt(customerSplit[9]);
+                String userName = customerSplit[10];
+                String password = customerSplit[11];
+                int PIN = Integer.parseInt(customerSplit[12]);
+                Customer newCustomer = new Customer(0, firstName, suffix, lastName, address, zipcode, city, email, sex, dob, BSN, userName, password, PIN);
+                customerList.add(newCustomer);
+
+            }
+        } catch (FileNotFoundException geenfile) {
+            System.out.println("Not found");
+        }
+    }
+}
