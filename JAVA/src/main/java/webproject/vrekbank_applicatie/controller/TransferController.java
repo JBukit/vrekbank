168e04e964fcfa5dd9f6967726bf2cada76d1608
Jacco <jjvdheuvel@hotmail.com>
2019-09-05 21:21:12 +0200

    transfer controller; transferobject ontvangt nu debitiban en in DB wordt alvast een transferrecord aangemaakt

--- JAVA/src/main/java/webproject/vrekbank_applicatie/controller/TransferController.java
+++ JAVA/src/main/java/webproject/vrekbank_applicatie/controller/TransferController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import webproject.vrekbank_applicatie.model.Transfer;
+import webproject.vrekbank_applicatie.service.AccountValidator;
 import webproject.vrekbank_applicatie.service.TransferValidator;
 
 @Controller
@@ -14,8 +15,15 @@ public class TransferController {
     @Autowired
     TransferValidator transferValidator;
 
+    @Autowired
+    AccountValidator accountValidator;
+
     @PostMapping(value = "TransferConfirmation")
     public String transferTransferConfirmation (@SessionAttribute("iban") String iban, @ModelAttribute Transfer transfer, Model model) {
+
+        // in transferobject iban van betaler opnemen
+
+        transfer.setDebitIban(iban);
 
 // poging tot doorgeven naar volgend scherm
         model.containsAttribute("iban");
@@ -30,6 +38,8 @@ public class TransferController {
 
         // in volgende ronde moet dit uitgebreid worden; if ontvanger geen vrekklant, dan de IBAN kan bestaan check.
 
+
+
         // model vullen uit transferobject
         model.addAttribute("debitIban", iban); // betaler
 
@@ -39,22 +49,27 @@ public class TransferController {
         model.addAttribute("description", transfer.getDescription());
         model.addAttribute("date", transfer.getDate());
 
+
+
+
         //uit tranferobject schrijven naar database in 3 stappen (volgorde?)
 
         //1.update tabel betaler
 
+        //nog een update functie in accountvalidator schrijven?
+        //accountValidator.
+
         //2. update tabel ontvanger (bij eigen klant)
+        //accountvalidator....
 
         //3. insert in tabel transfer
-        // transferValidator.saveTransfer(transfer);
+        transferValidator.saveTransfer(transfer);
 
         return "TransferConfirmation";
-
     }
 
     @GetMapping(value = "accountsummary")
     public String transferAccountSummaryHandler() {
         return "AccountSummary";
     }
-
 }
