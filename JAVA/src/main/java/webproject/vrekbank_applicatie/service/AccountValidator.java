d9d8bb56d465ad041b47c2c2a851eccc5c0382ac
Jacco <jjvdheuvel@hotmail.com>
2019-09-09 11:43:19 +0200

    transfercontroller naamgeving accountobject verhelderd en code wat opgeschoond en in openaccountcontroller magicnummers vervangen

--- JAVA/src/main/java/webproject/vrekbank_applicatie/service/AccountValidator.java
+++ JAVA/src/main/java/webproject/vrekbank_applicatie/service/AccountValidator.java
@@ -26,20 +26,21 @@ public class AccountValidator {
 
     // iban leidt tot account id, leidt tot balance
 
-    public void UpdateBalance (String iban, Transfer transfer) {
+    public void UpdateDebitBalance (String iban, Transfer transfer) {
     //1
-      Account a = accountDao.findByIban(iban);
+      Account payingaccount = accountDao.findByIban(iban);
         // 2
-        double balance = a.getBalance();
+        double balance = payingaccount.getBalance();
         // 3
         double newBalance = balance - transfer.getTransferAmount();
         // 4
-        a.setBalance(newBalance);
+        payingaccount.setBalance(newBalance);
         // 5 schrijven naar db
-        accountDao.save(a);
-
+        accountDao.save(payingaccount);
     }
-    // schrijven naar rekening ontvanger in zelfde functie of separaat?
+    // schrijven naar rekening ontvanger
+
+
 
 
 /*    public List<Account> findAccountsByCustomerId (int id) {
